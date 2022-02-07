93f2452ad1490b761941ac1d08b041b2d54ad02e
Lehaa <lucyan_bld@yahoo.com>
2022-02-07 15:24:02 +0200

    Added environment build job

--- variables.sh
+++ variables.sh
@@ -9,4 +9,14 @@ declare -F validate_environment_name  &>/dev/null || validate_environment_name()
     error "The \"environment_name\" parameter is mandatory"
     return 1
   fi
+
+  if [[ ${#environment_name} - gt 32 ]]; then
+    error "The \"environment_name\" parameter must have lenght less than or equal to 32"
+    return 1
+  fi
+
+  if [[ "$environment_name" =~ ^[A-Za-z0-9\-]+$ ]]; then
+    error "The \"environment_name\" parameter must only contain alphanumeric characters and dashes"
+    return 1
+  fi
 }
