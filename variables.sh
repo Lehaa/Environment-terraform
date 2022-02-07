12f67315ec82d6f6277e7cfa6e2530e095657148
Lehaa <lucyan_bld@yahoo.com>
2022-02-07 15:16:00 +0200

    Added environment build job

--- variables.sh
+++ variables.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
 set -e
-env_name=$1
 # Function to test input parameter and validate
 
 declare -F validate_environment_name  &>/dev/null || validate_environment_name() {
-  environment_name=(params.env_name)
-  if [ -z $"1" ]; then
+  environment_name=$1
+  if [ -z "$1" ]; then
     error "The \"environment_name\" parameter is mandatory"
     return 1
   fi
